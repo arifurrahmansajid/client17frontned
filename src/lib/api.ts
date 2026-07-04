b64513dc4d4ff8d836d17e71de56dce4c6210d27
Sabba Hossain <sabbahossain@gmail.com>
2026-07-04 15:58:18 +0600

    backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ||
   (process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
+    ? 'https://client17backend-pkr5.vercel.app'
     : 'https://client17backend-pkr5.vercel.app');
